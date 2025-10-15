b73c358308dab53e72e2fd21175da1374d64b0cd
Sebaslong <sguerrero.sgc@gmail.com>
2025-10-15 12:59:38 -0300

    Update to ssl api

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://23.95.3.178:8080';
+const API_BASE_URL = 'https://23.95.3.178:8443';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
